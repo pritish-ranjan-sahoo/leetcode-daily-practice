761fda48d1b9617b09bc36aadc779db3c708f286
Pritish Ranjan Sahoo <pritishsahoo8338@gmail.com>
2026-06-16 04:22:43 +0530

    Time: 4 ms (44.72%), Space: 51.8 MB (77.96%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,59 @@
+class Solution {
+    class Node{
+        int src;
+        int dest;
+        
+        Node(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public void bfs(char[][] grid, Node source){
+        Queue<Node> q = new ArrayDeque<>();
+        int m = grid.length - 1;
+        int n = grid[0].length - 1;
+        q.add(source);
+        while(!q.isEmpty()) {
+            Node first = q.remove();
+
+            // Top
+            if (first.src != 0 && grid[first.src - 1][first.dest] == '1') {
+                grid[first.src - 1][first.dest] = '2';
+                q.add(new Node(first.src - 1, first.dest));
+            }
+
+            // bottom
+            if (first.src != m && grid[first.src + 1][first.dest] == '1') {
+                grid[first.src + 1][first.dest] = '2';
+                q.add(new Node(first.src + 1, first.dest));
+            }
+
+            // left
+            if (first.dest != 0 && grid[first.src][first.dest - 1] == '1') {
+                grid[first.src][first.dest - 1] = '2';
+                q.add(new Node(first.src, first.dest - 1));
+            }
+
+            // right
+            if (first.dest != n && grid[first.src][first.dest + 1] == '1') {
+                grid[first.src][first.dest + 1] = '2';
+                q.add(new Node(first.src, first.dest + 1));
+            }
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid,new Node(i,j));
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
